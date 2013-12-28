72e777b57905bb7637044502debf1622e043c603
Wolfgang Wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2013-12-28 22:50:08 +0100

    Testfall an *.csv-Export angepasst

--- test.php
+++ test.php
@@ -5,15 +5,17 @@ $_SERVER = array();
 # Basis
 $_SERVER['REMOTE_USER'] = 'test';
 # Test 1
-$_REQUEST['controller'] = 'konto';
-$_REQUEST['action'] = 'list';
+#$_REQUEST['controller'] = 'konto';
+#$_REQUEST['action'] = 'list';
 # Test 2
 #$_REQUEST['controller'] = 'buchung';
 #$_REQUEST['action'] = 'listbykonto';
 #$_REQUEST['konto'] = '2800';
 # Test 3
-#$_REQUEST['controller'] = 'ergebnis';
-#$_REQUEST['action'] = 'guv';
+$_REQUEST['controller'] = 'ergebnis';
+$_REQUEST['action'] = 'guv';
+$_REQUEST['outputtype'] = 'csv';
+
 include("./index.php");
 
 ?>
