820550683f3e0e5a0c916176d451e55715ded980
Wolfgang Wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2014-02-27 16:01:02 +0100

    Das zusätzliche Feld bearbeiteR_user_id auch im create-Skript für die Neuinstallation mit berücksichtigen.

--- sql/create-tables-and-views.sql
+++ sql/create-tables-and-views.sql
@@ -11,6 +11,7 @@ CREATE TABLE IF NOT EXISTS `fi_buchungen` (
   `habenkonto` varchar(20) NOT NULL,
   `betrag` decimal(10,2) NOT NULL,
   `datum` date NOT NULL,
+  `bearbeiter_user_id` integer DEFAULT NULL,
   PRIMARY KEY (`buchungsnummer`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
