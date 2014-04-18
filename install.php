452f117efe18e5f7fcef47fe2de2e5a86fe7c85a
Wolfgang Wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2014-04-18 12:33:43 +0200

    Grundlagen für Installer vorbereitet

---
+++ install.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+<meta name="viewport" content="width:device-width, initial-scale=1">
+<link rel="stylesheet" href="./html/css/jquery.mobile-1.3.1.css"/>
+<!-- Standard-Bibliotheken -->
+<script src="./html/js/jquery-2.0.2.min.js"></script>
+<script src="./html/js/jquery.mobile-1.3.1.js"></script>
+<script src="./html/js/knockout-2.2.1.js"></script>
+<!-- Installer-spezifische Code-Dateien -->
+<!-- TODO: knockout-model für Installer -->
+<title>html5-haushaltsbuch Installation</title>
+</head>
+<body>
+<div data-role="page" id="startseite">
+<div data-role="header" data-theme="b">
+<h1>html5-haushaltsbuch Installation</h1>
+</div>
+<div data-role="content">
+Installationsprogramm für das HTML5-Haushaltsbuch.
+</div>
+</div>
+</body>
+</html>
