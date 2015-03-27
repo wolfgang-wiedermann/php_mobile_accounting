9c78b13a045acb677de911081b46553609494f6b
Wolfgang Wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2015-03-27 17:03:04 +0000

    Mandant zur Bilanzberechnung hinzugefügt

--- sql/query/bilanz.sql
+++ sql/query/bilanz.sql
@@ -7,15 +7,19 @@ left outer join
 (
   select sollkonto as konto, 'S' as buchungstyp, sum(betrag) as betrag
   from fi_buchungen
+  where mandant_id = #mandant_id#
   group by sollkonto, 'S'
 ) as soll
 on konten.kontonummer = soll.konto
+and konten.mandant_id = #mandant_id#
 left outer join
 (
   select habenkonto as konto, 'H' as buchungstyp, sum(betrag) as betrag
   from fi_buchungen
+  where mandant_id = #mandant_id#
   group by habenkonto, 'H'
 ) as haben
 on konten.kontonummer = haben.konto
+and konten.mandant_id = #mandant_id#
 where konten.kontenart_id in (1, 2) 
 and (soll.konto is not null or haben.konto is not null) 
